4c29943ee9bc65ede10830f20c3d997aaca176c8
Wei Yang <wei@weizilla.com>
2015-12-17 04:32:54 +0000

    add powertail

--- lights.py
+++ lights.py
@@ -2,20 +2,33 @@
 
 import time
 import Adafruit_CharLCD as LCD
+import RPi.GPIO as io
 
-lcd = LCD.Adafruit_CharLCDPlate()
 backlight = True
+powertail = False
 activate = True
+powerPin = 23
+
+io.setmode(io.BCM)
+io.setup(powerPin, io.OUT)
+lcd = LCD.Adafruit_CharLCDPlate()
+lcd.set_color(1.0, 0.0, 0.0)
 
 def toggleBacklight():
     global backlight
-    backlight = not backlight
     lcd.set_backlight(backlight)
+    backlight = not backlight
+
+def togglePowerTail():
+    global powertail
+    io.output(powerPin, powertail) 
+    powertail = not powertail
 
 while True:
     if lcd.is_pressed(LCD.SELECT):
         if activate == True:
             toggleBacklight() 
+            togglePowerTail()
         activate = False
     else:
         activate = True
