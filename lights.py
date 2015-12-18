2beba1d3aa5d23472d0c1df8fc8662a2bea0d6d6
Wei Yang <wei@weizilla.com>
2015-12-18 03:59:48 +0000

    add lights

--- lights.py
+++ lights.py
@@ -3,34 +3,36 @@
 import time
 import Adafruit_CharLCD as LCD
 import RPi.GPIO as io
+import schedule
 
 buttons = (LCD.SELECT, LCD.LEFT, LCD.UP, LCD.DOWN, LCD.RIGHT)
 
-class MessageDisplay(object):
-    def __init__(self, lcd):
-        self.lcd = lcd
-        self.num = 0
+class Lights(object):
+    def __init__(self):
+        self.on_time = "5:45"
+        schedule.every().day.at(self.on_time).do(self.light_on)
 
-    def message_1(self):
-        def msg():
-            self.lcd.clear()
-            self.lcd.message("exec 1 {}".format(self.num))
-        return ("Message 1", msg)
+        self.off_time = "6:30"
+        schedule.every().day.at(self.off_time).do(self.light_off)
 
-    def message_2(self):
-        def msg():
-            self.lcd.clear()
-            self.lcd.message("exec 2 {}".format(self.num))
-        return ("Message 2", msg)
+        self.powertail_pin = 23
+        io.setmode(io.BCM)
+        io.setup(self.powertail_pin, io.OUT)
 
-    def message_3(self):
-        def msg():
-            self.lcd.clear()
-            self.lcd.message("exec 3 {}".format(self.num))
-        return ("Message 3", msg)
+    def light_on(self):
+        io.output(self.powertail_pin, True) 
 
-    def update(self):
-        self.num += 1
+    def light_off(self):
+        io.output(self.powertail_pin, False)
+
+    def light_on_menu(self):
+        return ("On Time {}".format(self.on_time), self.light_on)
+
+    def light_off_menu(self):
+        return ("Off Time {}".format(self.off_time), self.light_off)
+
+    def run_pending(self):
+        schedule.run_pending()
 
 class Menu(object):
     def __init__(self, lcd):
@@ -40,18 +42,10 @@ class Menu(object):
         self.lcd = lcd
 
         self.backlight = True
-        self.powertail = False
-        self.powertail_pin = 23
-        io.setmode(io.BCM)
-        io.setup(self.powertail_pin, io.OUT)
 
     def toggle_backlight(self):
         self.lcd.set_backlight(self.backlight)
         self.backlight = not self.backlight
-
-    def togglePowerTail():
-        io.output(self.powertail_pin, self.powertail) 
-        self.powertail = not self.powertail
 
     def add_update_listener(self, item):
         self.updates.append(item) 
@@ -117,12 +111,11 @@ class Menu(object):
 
 if __name__ == "__main__":
     lcd = LCD.Adafruit_CharLCDPlate()
-    msg = MessageDisplay(lcd)
+    lights = Lights()
 
     menu = Menu(lcd)
-    menu.add_menu_item(msg.message_1())
-    menu.add_menu_item(msg.message_2())
-    menu.add_menu_item(msg.message_3())
-    menu.add_update_listener(msg.update)
+    menu.add_menu_item(lights.light_on_menu())
+    menu.add_menu_item(lights.light_off_menu())
+    menu.add_update_listener(lights.run_pending)
 
     menu.start()
