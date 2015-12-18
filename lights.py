b322d2b9e1ee64f1f4897b77af788376e9c9ca60
Wei Yang <wei@weizilla.com>
2015-12-18 02:41:07 +0000

    add menu

--- lights.py
+++ lights.py
@@ -4,33 +4,106 @@ import time
 import Adafruit_CharLCD as LCD
 import RPi.GPIO as io
 
-backlight = True
-powertail = False
-activate = True
-powerPin = 23
+buttons = (LCD.SELECT, LCD.LEFT, LCD.UP, LCD.DOWN, LCD.RIGHT)
 
-io.setmode(io.BCM)
-io.setup(powerPin, io.OUT)
-lcd = LCD.Adafruit_CharLCDPlate()
-lcd.set_color(1.0, 0.0, 0.0)
+class MessageDisplay(object):
+    def __init__(self, lcd):
+        self.lcd = lcd
 
-def toggleBacklight():
-    global backlight
-    lcd.set_backlight(backlight)
-    backlight = not backlight
+    def message_1(self):
+        def msg():
+            self.lcd.clear()
+            self.lcd.message("exec 1")
+        return ("Message 1", msg)
 
-def togglePowerTail():
-    global powertail
-    io.output(powerPin, powertail) 
-    powertail = not powertail
+    def message_2(self):
+        def msg():
+            self.lcd.clear()
+            self.lcd.message("exec 2")
+        return ("Message 2", msg)
 
-while True:
-    if lcd.is_pressed(LCD.SELECT):
-        if activate == True:
-            toggleBacklight() 
-            togglePowerTail()
-        activate = False
-    else:
+    def message_3(self):
+        def msg():
+            self.lcd.clear()
+            self.lcd.message("exec 3")
+        return ("Message 3", msg)
+
+class Menu(object):
+    def __init__(self, lcd):
+        self.current = 0
+        self.menus = []
+        self.lcd = lcd
+
+        self.backlight = True
+        self.powertail = False
+        self.powertail_pin = 23
+        io.setmode(io.BCM)
+        io.setup(self.powertail_pin, io.OUT)
+
+    def toggle_backlight(self):
+        self.lcd.set_backlight(self.backlight)
+        self.backlight = not self.backlight
+
+    def togglePowerTail():
+        io.output(self.powertail_pin, self.powertail) 
+        self.powertail = not self.powertail
+
+    def add_item(self, item):
+        self.menus.append(item)
+
+    def button_pressed(self, button):
+        if button == LCD.SELECT:
+            self.toggle_backlight()
+        elif button == LCD.UP:
+            self.current += 1
+            if self.current == len(self.menus):
+                self.current = 0
+            self.update_menu()
+        elif button == LCD.DOWN:
+            self.current -= 1 
+            if self.current == -1:
+                self.current = len(self.menus) - 1
+            self.update_menu()
+        elif button == LCD.RIGHT:
+            self.exec_menu()
+        elif button == LCD.LEFT:
+            self.update_menu()
+
+    def update_menu(self):
+        item = self.menus[self.current]
+        lcd.clear()
+        lcd.message(item[0])
+
+    def exec_menu(self):
+        item = self.menus[self.current]
+        item[1]()
+
+    def is_pressed(self):
+        for button in buttons:
+            if self.lcd.is_pressed(button):
+                return button
+
+    def start(self):
+        self.update_menu()
         activate = True
+        while True:
+            button = self.is_pressed()
+            if button is not None:
+                if activate:
+                    self.button_pressed(button)
+                activate = False
+            else:
+                activate = True
 
-    time.sleep(0.1)  
+            time.sleep(0.1)  
+
+if __name__ == "__main__":
+    lcd = LCD.Adafruit_CharLCDPlate()
+    msg = MessageDisplay(lcd)
+
+    menu = Menu(lcd)
+    menu.add_item(msg.message_1())
+    menu.add_item(msg.message_2())
+    menu.add_item(msg.message_3())
+
+    menu.start()
