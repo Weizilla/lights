adc8dba0f39fe1c5e62e6050f26d1bbd4705b267
Wei Yang <wei@weizilla.com>
2016-01-30 20:48:26 +0000

    fix bugs with lights pi

--- lights_pi.py
+++ lights_pi.py
@@ -2,7 +2,8 @@
 
 import time
 import importlib
-rpi_spec = importlib.util.find_spec("RPi")
+rpi_spec = importlib.find_loader("RPi")
+#rpi_spec = importlib.util.find_spec("RPi")
 if rpi_spec is not None:
     import RPi.GPIO as io
 else:
@@ -19,24 +20,20 @@ class LightsPi(Lights):
         if io:
             io.setmode(io.BCM)
             io.setup(BIG_BUTTON, io.IN, pull_up_down=io.PUD_UP)
-            io.setup(POWERTAIL, io.OUT)
+            io.setup(POWERTAIL, io.OUT, initial=0)
 
     def update_state(self):
         io.output(POWERTAIL, self.get_light())
+        pass
 
     def run_pending(self):
         # schedule.run_pending()
         pass
 
-    def is_big_pressed(self):
-        if io.input(BIG_BUTTON):
-            return BIG_BUTTON
-
     def start(self):
-        big_activate = True
+        big_activate = False
         while True:
-            button = self.is_big_pressed()
-            if button is not None:
+            if io.input(BIG_BUTTON):
                 if big_activate:
                     self.toggle_light()
                 big_activate = False
@@ -48,9 +45,12 @@ class LightsPi(Lights):
             try:
                 time.sleep(0.1)
             except KeyboardInterrupt:
-                print("Goodbye")
-                io.cleanup()
-                exit(0)
+                self.stop()
+
+    def stop(self):
+        print("Goodbye")
+        io.cleanup()
+        exit(0)
 
 if __name__ == "__main__":
     lights = LightsPi()
