c825949f20ef35bbf26f8a492a695670e87724f3
Wei Yang <wei@weizilla.com>
2016-01-30 16:49:15 -0600

    add web ui

--- static/js/app.js
+++ static/js/app.js
@@ -3,5 +3,26 @@ app = angular.module("lights-app", []);
 app.controller("LightsController", function($http) {
     var self = this;
     self.lights = "LIGHTS";
+
+    self.toggle = function() {
+        $http.get("/api/toggle").success(function(data) {
+            self.getLights();
+        });
+    };
+
+    self.setLights = function(state) {
+        $http.put("/api/lights", {lights: state}).success(function(data) {
+            self.getLights();
+        })
+    };
+
+    self.getLights = function() {
+        $http.get("api/lights").success(function(data) {
+            self.lights = data["light"];
+        });
+    };
+
+    self.getLights();
+
     return self;
 });
