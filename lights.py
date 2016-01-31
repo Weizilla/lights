ccb0b22198c97c9755b103b411806acffa2b0f64
Wei Yang <wei@weizilla.com>
2016-01-31 08:56:51 -0600

    add scheduling for starting single on/off job

--- lights.py
+++ lights.py
@@ -1,11 +1,15 @@
 import time
-
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
 
 class Lights:
     def __init__(self):
         self._light = False
         self._times = {}
+        self._jobs = {}
         self._debounce = None
+        self._scheduler = BackgroundScheduler()
+        self._scheduler.start()
 
     def toggle_light(self):
         self.set_light(not self.get_light())
@@ -33,12 +37,32 @@ class Lights:
 
     def set_time(self, mode, trigger_time):
         print("setting time for mode {}: {}".format(mode, trigger_time))
-        self._times[mode] = time.strptime(trigger_time, "%H:%M")
+
+        light_map = {"on": True, "off": False}
+        if mode not in light_map:
+            print("Unexpected mode:", mode)
+            return
+
+        self._times[mode] = trigger_time
+        next_time = self._calc_next_time(trigger_time)
+        light = light_map[mode]
+        job = self._scheduler.add_job(self.set_light, args=[light], next_run_time=next_time)
+        self._replace_prev_job(mode, job.id)
+        print("started job {} for mode {} to run at {}".format(job.id, mode, next_time))
+
+    def _calc_next_time(self, time):
+        h, m = [int(t) for t in time.split(":")]
+        now = datetime.now()
+        next_time = now.replace(hour=h, minute=m, second=0, microsecond=0)
+        if next_time < now:
+            next_time += timedelta(days=1)
+        return next_time
+
+    def _replace_prev_job(self, mode, new_id):
+        if mode in self._jobs:
+            old_id = self._jobs[mode]
+            self._scheduler.remove_job(old_id)
+        self._jobs[mode] = new_id
 
     def get_times(self):
-        return {k : time.strftime("%H:%M", v) for (k, v) in self._times.items()}
-
-    def get_time(self, mode):
-        light = self._times.get(mode)
-        print("getting time for mode {} : {}".format(mode, light))
-        return light
+        return self._times
