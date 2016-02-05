67d5d3a3cff79d11015c75710dfffd891aba37a1
Wei Yang <wei@weizilla.com>
2016-02-04 23:12:34 -0600

    change scheduler to cron trigger and add tests

--- lights/lights.py
+++ lights/lights.py
@@ -1,17 +1,17 @@
 import time
+from collections import namedtuple
 from datetime import datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
 class Lights:
-    def __init__(self, state_callback=None):
+    def __init__(self, state_callback=None, scheduler=None):
         self._state = False
         self._debounce = None
         self._state_callback = state_callback
-        # self._times = {}
-        # self._jobs = {}
-        # self._scheduler = BackgroundScheduler()
-        # self._scheduler.start()
+        self._scheduler = scheduler or BackgroundScheduler()
+        self._scheduler.start()
+        self._triggers = {}
 
     @property
     def state(self):
@@ -28,34 +28,27 @@ class Lights:
     def toggle(self):
         self.state = not self.state
 
-    # def set_time(self, mode, trigger_time):
-    #     print("setting time for mode {}: {}".format(mode, trigger_time))
-    #
-    #     light_map = {"on": True, "off": False}
-    #     if mode not in light_map:
-    #         print("Unexpected mode:", mode)
-    #         return
-    #
-    #     self._times[mode] = trigger_time
-    #     next_time = self._calc_next_time(trigger_time)
-    #     light = light_map[mode]
-    #     job = self._scheduler.add_job(self.set_light, args=[light], next_run_time=next_time)
-    #     self._replace_prev_job(mode, job.id)
-    #     print("started job {} for mode {} to run at {}".format(job.id, mode, next_time))
-    #
-    # def _calc_next_time(self, time):
-    #     h, m = [int(t) for t in time.split(":")]
-    #     now = datetime.now()
-    #     next_time = now.replace(hour=h, minute=m, second=0, microsecond=0)
-    #     if next_time < now:
-    #         next_time += timedelta(days=1)
-    #     return next_time
-    #
-    # def _replace_prev_job(self, mode, new_id):
-    #     if mode in self._jobs:
-    #         old_id = self._jobs[mode]
-    #         self._scheduler.remove_job(old_id)
-    #     self._jobs[mode] = new_id
-    #
-    # def get_times(self):
-    #     return self._times
+    def _set_state(self, state):
+        self.state = state
+
+    def add_trigger(self, state, hour, minute, repeat_weekday=False, repeat_weekend=False):
+        tomorrow_now = datetime.now() + timedelta(days=1)
+        job = self._scheduler.add_job(self._set_state, args=[state], trigger="cron", hour=hour,
+                                      minute=minute, end_date=tomorrow_now)
+        trigger = Trigger(job_id=job.id, state=state, hour=hour, minute=minute,
+                          repeat_weekday=repeat_weekday,
+                          repeat_weekend=repeat_weekend)
+        self._triggers[job.id] = trigger
+
+    @property
+    def triggers(self):
+        active_triggers = {}
+        for job in self._scheduler.get_jobs():
+            job_id = job.id
+            active_triggers[job_id] = self._triggers[job_id]
+        self._triggers = active_triggers
+        return list(self._triggers.values())
+
+
+class Trigger(namedtuple("Trigger", "job_id state hour minute repeat_weekday repeat_weekend")):
+    pass
---
+++ tests/lights-scheduler-test.py
@@ -0,0 +1,78 @@
+from hamcrest import *
+from unittest import TestCase
+from unittest.mock import Mock
+from lights.lights import Lights
+
+
+class TestLightsScheduler(TestCase):
+
+    def setUp(self):
+        self.lights = Lights()
+
+    def test_starts_scheduler(self):
+        mock = Mock()
+
+        Lights(scheduler=mock)
+        mock.start.assert_called_with()
+
+    def test_add_trigger_and_get_job(self):
+        state = True
+        hour = 10
+        minute = 20
+        self.lights.add_trigger(state, hour, minute)
+
+        jobs = self.lights._scheduler.get_jobs()
+        assert_that(len(jobs), is_(1))
+
+    def test_add_triggers_and_get_jobs(self):
+        num = 5
+        for i in range(num):
+            self.lights.add_trigger(True, 10, 20)
+
+        jobs = self.lights._scheduler.get_jobs()
+        assert_that(len(jobs), is_(num))
+
+        triggers = self.lights.triggers
+        assert_that(len(triggers), is_(num))
+
+    def test_add_and_get_trigger(self):
+        state = True
+        hour = 10
+        minute = 20
+        self.lights.add_trigger(state, hour, minute)
+
+        job_id = self.lights._scheduler.get_jobs()[0].id
+        triggers = self.lights.triggers
+        assert_that(len(triggers), is_(1))
+
+        trigger = triggers[0]
+        assert_that(trigger.job_id, is_(job_id))
+        assert_that(trigger.state, is_(state))
+        assert_that(trigger.hour, is_(hour))
+        assert_that(trigger.minute, is_(minute))
+        assert_that(trigger.repeat_weekday, is_(False))
+        assert_that(trigger.repeat_weekend, is_(False))
+
+    def test_add_no_repeat_has_end_date(self):
+        self.lights.add_trigger(True, 10, 20)
+
+        job = self.lights._scheduler.get_jobs()[0]
+        assert_that(job.trigger.end_date, is_(not_none()))
+
+    def test_remove_finished_jobs_from_trigger(self):
+        self.lights.add_trigger(True, 10, 20)
+        self.lights.add_trigger(True, 10, 20)
+
+        jobs = self.lights._scheduler.get_jobs()
+        removed_job_id = jobs[0].id
+        keep_job_id = jobs[1].id
+
+        self.lights._scheduler.remove_job(removed_job_id)
+        jobs = self.lights._scheduler.get_jobs()
+        assert_that(len(jobs), is_(1))
+
+        triggers = self.lights.triggers
+        assert_that(len(triggers), is_(1))
+
+        trigger = triggers[0]
+        assert_that(trigger.job_id, is_(keep_job_id))
