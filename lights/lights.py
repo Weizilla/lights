b3988ba89248d827cad4605fd72ac7003cecab1a
Wei Yang <wei@weizilla.com>
2016-02-04 21:11:02 -0600

    add unit test for just bare setter functionality

--- lights/lights.py
+++ lights/lights.py
@@ -2,67 +2,60 @@ import time
 from datetime import datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 
+
 class Lights:
-    def __init__(self):
-        self._light = False
-        self._times = {}
-        self._jobs = {}
+    def __init__(self, state_callback=None):
+        self._state = False
         self._debounce = None
-        self._scheduler = BackgroundScheduler()
-        self._scheduler.start()
+        self._state_callback = state_callback
+        # self._times = {}
+        # self._jobs = {}
+        # self._scheduler = BackgroundScheduler()
+        # self._scheduler.start()
 
-    def toggle_light(self):
-        self.set_light(not self.get_light())
-        return self.get_light();
+    @property
+    def state(self):
+        return self._state
 
-    def set_light(self, light):
-        if self._update_state(light):
-            self._light = light
-        return self.get_light()
-
-    def get_light(self):
-        return self._light
-
-    def _update_state(self, new_state):
+    @state.setter
+    def state(self, value):
         if self._debounce is None or (time.time() - self._debounce > 1):
-            self.set_state(new_state)
+            if self._state_callback:
+                self._state_callback(value)
+            self._state = value
             self._debounce = time.time()
-            return True
-        return False
 
-    def set_state(self, new_state):
-        # to be overwritten by hardware
-        print("setting state to {}".format(new_state))
-        pass
+    def toggle(self):
+        self.state = not self.state
 
-    def set_time(self, mode, trigger_time):
-        print("setting time for mode {}: {}".format(mode, trigger_time))
-
-        light_map = {"on": True, "off": False}
-        if mode not in light_map:
-            print("Unexpected mode:", mode)
-            return
-
-        self._times[mode] = trigger_time
-        next_time = self._calc_next_time(trigger_time)
-        light = light_map[mode]
-        job = self._scheduler.add_job(self.set_light, args=[light], next_run_time=next_time)
-        self._replace_prev_job(mode, job.id)
-        print("started job {} for mode {} to run at {}".format(job.id, mode, next_time))
-
-    def _calc_next_time(self, time):
-        h, m = [int(t) for t in time.split(":")]
-        now = datetime.now()
-        next_time = now.replace(hour=h, minute=m, second=0, microsecond=0)
-        if next_time < now:
-            next_time += timedelta(days=1)
-        return next_time
-
-    def _replace_prev_job(self, mode, new_id):
-        if mode in self._jobs:
-            old_id = self._jobs[mode]
-            self._scheduler.remove_job(old_id)
-        self._jobs[mode] = new_id
-
-    def get_times(self):
-        return self._times
+    # def set_time(self, mode, trigger_time):
+    #     print("setting time for mode {}: {}".format(mode, trigger_time))
+    #
+    #     light_map = {"on": True, "off": False}
+    #     if mode not in light_map:
+    #         print("Unexpected mode:", mode)
+    #         return
+    #
+    #     self._times[mode] = trigger_time
+    #     next_time = self._calc_next_time(trigger_time)
+    #     light = light_map[mode]
+    #     job = self._scheduler.add_job(self.set_light, args=[light], next_run_time=next_time)
+    #     self._replace_prev_job(mode, job.id)
+    #     print("started job {} for mode {} to run at {}".format(job.id, mode, next_time))
+    #
+    # def _calc_next_time(self, time):
+    #     h, m = [int(t) for t in time.split(":")]
+    #     now = datetime.now()
+    #     next_time = now.replace(hour=h, minute=m, second=0, microsecond=0)
+    #     if next_time < now:
+    #         next_time += timedelta(days=1)
+    #     return next_time
+    #
+    # def _replace_prev_job(self, mode, new_id):
+    #     if mode in self._jobs:
+    #         old_id = self._jobs[mode]
+    #         self._scheduler.remove_job(old_id)
+    #     self._jobs[mode] = new_id
+    #
+    # def get_times(self):
+    #     return self._times
---
+++ tests/lights-state-test.py
@@ -0,0 +1,72 @@
+from hamcrest import *
+from lights.lights import Lights
+from freezegun import freeze_time
+from datetime import datetime, timedelta
+from unittest.mock import Mock
+from unittest import TestCase
+
+
+class TestLightsState(TestCase):
+    def setUp(self):
+        self.lights = Lights()
+
+    def test_state_starts_as_false(self):
+        assert_that(self.lights.state, is_(False))
+
+    def test_set_and_return_state(self):
+        self.lights.state = True
+        assert_that(self.lights.state, is_(True))
+
+    def test_toggle_flips_state(self):
+        assert_that(self.lights.state, is_(False))
+
+        self.lights.toggle()
+        assert_that(self.lights.state, is_(True))
+
+    def test_set_state_does_not_change_if_less_than_bounce_duration(self):
+        self.lights.state = True
+        assert_that(self.lights.state, is_(True))
+
+        self.lights.state = False
+        assert_that(self.lights.state, is_(True))
+
+    def test_set_state_changes_if_more_than_bounce_duration(self):
+        with freeze_time(datetime.now()) as curr_time:
+            self.lights.state = True
+            assert_that(self.lights.state, is_(True))
+
+            curr_time.tick(delta=timedelta(seconds=90))
+
+            self.lights.state = False
+            assert_that(self.lights.state, is_(False))
+
+    def test_toggle_does_not_change_if_less_than_bounce_duration(self):
+        self.lights.toggle()
+        assert_that(self.lights.state, is_(True))
+
+        self.lights.toggle()
+        assert_that(self.lights.state, is_(True))
+
+    def test_toggle_changes_if_more_than_bounce_duration(self):
+        with freeze_time(datetime.now()) as curr_time:
+            self.lights.toggle()
+            assert_that(self.lights.state, is_(True))
+
+            curr_time.tick(delta=timedelta(seconds=90))
+
+            self.lights.toggle()
+            assert_that(self.lights.state, is_(False))
+
+    def test_set_state_calls_callback(self):
+        mock = Mock()
+        lights = Lights(mock.state)
+
+        lights.state = True
+        mock.state.assert_called_with(True)
+
+    def test_toggle_calls_callback(self):
+        mock = Mock()
+        lights = Lights(mock.state)
+
+        lights.toggle()
+        mock.state.assert_called_with(True)
