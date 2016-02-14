42bcec0195c039ae0bf9f6fc902e19f0f1d6d238
Wei Yang <wei@weizilla.com>
2016-02-13 22:39:28 -0600

    refactor js for new trigger class

--- static/js/app.js
+++ static/js/app.js
@@ -4,32 +4,7 @@ app.controller("LightsController", function($http) {
     var self = this;
     self.lights = "LIGHTS";
     self.triggers = [];
-    self.newTrigger = {
-        state: true,
-        repeatWeekday: false,
-        repeatWeekend: false,
-        stateName: function() {
-            return this.state ? "ON" : "OFF";
-        },
-        toggleState: function() {
-            this.state = ! this.state;
-        },
-        toggleRepeatWeekday: function() {
-            this.repeatWeekday = ! this.repeatWeekday;
-        },
-        toggleRepeatWeekend: function() {
-            this.repeatWeekend = ! this.repeatWeekend;
-        },
-        asData: function() {
-            return {
-                state: this.state,
-                hour: moment(this.time).hour(),
-                minute: moment(this.time).minute(),
-                repeat_weekday: this.repeatWeekday,
-                repeat_weekend: this.repeatWeekend,
-            }
-        }
-    };
+    self.newTrigger = new Trigger();
 
     self.toggle = function() {
         $http.get("/api/toggle").success(function(data) {
@@ -52,9 +27,7 @@ app.controller("LightsController", function($http) {
     self.updateTriggers = function() {
         $http.get("api/triggers").success(function(data) {
             self.triggers = data.map(function(data) {
-                data.time = moment({hour: data.hour, minute: data.minute}).format("h:mm a");
-                data.next_run_time = moment(data.next_run_time * 1000).toDate();
-                return data
+                return new Trigger(data);
             });
             self.triggers.sort(function(a, b) {
                 return a.next_run_time - b.next_run_time;
@@ -64,6 +37,7 @@ app.controller("LightsController", function($http) {
 
     self.addTrigger = function() {
         $http.put("api/triggers", self.newTrigger.asData()).success(function(data) {
+            self.newTrigger = new Trigger();
             self.updateTriggers();
         });
     };
---
+++ static/js/trigger.js
@@ -0,0 +1,33 @@
+function Trigger(input) {
+    this.state = true;
+    this.repeatWeekday = false;
+    this.repeatWeekend = false;
+    if (input !== undefined) {
+        this.state = input.state;
+        this.repeatWeekday = input.repeat_weekday;
+        this.repeatWeekend = input.repeat_weekend;
+        this.time = moment({hour: input.hour, minute: input.minute}).format("h:mm a");
+        this.next_run_time = moment(input.next_run_time * 1000).toDate();
+    }
+    this.stateName = function() {
+        return this.state ? "ON" : "OFF";
+    };
+    this.toggleState = function() {
+        this.state = !this.state;
+    };
+    this.toggleRepeatWeekday = function() {
+        this.repeatWeekday = !this.repeatWeekday;
+    };
+    this.toggleRepeatWeekend = function () {
+        this.repeatWeekend = !this.repeatWeekend;
+    };
+    this.asData = function() {
+        return {
+            state: this.state,
+            hour: moment(this.time).hour(),
+            minute: moment(this.time).minute(),
+            repeat_weekday: this.repeatWeekday,
+            repeat_weekend: this.repeatWeekend,
+        }
+    }
+}
