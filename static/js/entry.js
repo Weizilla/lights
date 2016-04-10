86de9257779406824a1515f75468c2185d13007a
Wei Yang <wei@weizilla.com>
2016-04-10 17:27:19 -0500

    improve ui to use flexbox

--- static/js/entry.js
+++ static/js/entry.js
@@ -1,6 +1,6 @@
 function Entry(input) {
     this.datetime = moment(input.timestamp * 1000).toDate();
     this.timestamp = input.timestamp;
-    this.state = input.state;
+    this.state = input.state === "True";
     this.source = input.source;
 }
